41f9c33f50f38eea8143725d817f8ab1271e429e
kiên vũ <vukien104@gmail.com>
2016-09-08 16:24:51 +0700

    add home screen

---
+++ Pokemon Quiz'Em All/HomeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  HomeViewController.swift
+//  Pokemon Quiz'Em All
+//
+//  Created by Vũ Kiên on 08/09/2016.
+//  Copyright © 2016 Kiên Vũ. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var settingBtn: UIButton!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var scoreLbl: UILabel!
+    @IBOutlet weak var highScoreLbl: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+    }
+    
+    func setupView(){
+        
+    }
+    @IBAction func playDidTapped(sender: UIButton) {
+    }
+    @IBAction func settingDidTapped(sender: UIButton) {
+    }
+}
